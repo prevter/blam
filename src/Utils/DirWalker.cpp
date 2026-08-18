069de2ba3625ca834fcaa0bd2eabacfad76aabc2
prevter <prevter@gmail.com>
2026-08-18 15:37:29 +0300

    fix musl support

--- src/Utils/DirWalker.cpp
+++ src/Utils/DirWalker.cpp
@@ -20,8 +20,8 @@
     #include <sys/syscall.h>
 
     struct Dirent64 {
-        ino64_t d_ino;
-        off64_t d_off;
+        ino_t d_ino;
+        off_t d_off;
         unsigned short d_reclen;
         unsigned char d_type;
         char d_name[];
