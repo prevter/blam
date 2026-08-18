8cb8bd4a0af5dd40f6456d663c121dff36c54fd3
prevter <prevter@gmail.com>
2026-08-19 00:09:18 +0300

    fix simd detection

--- src/SIMD/Common.cpp
+++ src/SIMD/Common.cpp
@@ -1,25 +1,30 @@
 #include "Common.hpp"
 
 #ifdef BLAM_PLATFORM_X86_64
-
-#if defined(_MSC_VER)
-#include <intrin.h>
-#endif
-
-static void cpuid(int info[4], int infoType) noexcept {
     #if defined(_MSC_VER)
-    __cpuid(info, infoType);
+        #include <intrin.h>
     #elif defined(__GNUC__) || defined(__clang__)
-    __asm__ __volatile__(
-        "cpuid"
-        : "=a" (info[0]), "=b" (info[1]), "=c" (info[2]), "=d" (info[3])
-        : "a" (infoType)
-    );
+        #include <cpuid.h>
+        #include <x86intrin.h>
     #endif
-}
+
+    static uint64_t get_xcr0() noexcept {
+    #if defined(_MSC_VER)
+        return _xgetbv(0);
+    #else
+        uint32_t eax, edx;
+        asm volatile("xgetbv" : "=a"(eax), "=d"(edx) : "c"(0));
+        return (static_cast<uint64_t>(edx) << 32) | eax;
+    #endif
+    }
 #elif defined(BLAM_PLATFORM_ARM64)
-// #include <sys/auxv.h>
-// #include <asm/hwcap.h>
+    #ifdef BLAM_PLATFORM_LINUX
+        #include <asm/hwcap.h>
+        #include <sys/auxv.h>
+    #elif defined(BLAM_PLATFORM_MACOS)
+        #include <sys/sysctl.h>
+        #include <sys/types.h>
+    #endif
 #endif
 
 namespace blam::simd {
@@ -27,41 +32,71 @@ namespace blam::simd {
         static CPUFeatures features = [] {
             CPUFeatures f;
         #ifdef BLAM_PLATFORM_X86_64
+        #if defined(_MSC_VER)
             int info[4];
-            cpuid(info, 0);
+            __cpuid(info, 0);
             int nIds = info[0];
+        #else
+            unsigned int eax, ebx, ecx, edx;
+            if (!__get_cpuid(0, &eax, &ebx, &ecx, &edx)) return f;
+            int nIds = static_cast<int>(eax);
+        #endif
 
             bool avx_cpu = false;
             bool avx_os  = false;
 
             if (nIds >= 1) {
-                cpuid(info, 1);
-                f.sse2 = (info[3] & (1 << 26)) != 0;
+            #if defined(_MSC_VER)
+                __cpuid(info, 1);
+                f.sse2  = (info[3] & (1 << 26)) != 0;
                 avx_cpu = (info[2] & (1 << 28)) != 0;
                 bool osxsave = (info[2] & (1 << 27)) != 0;
-                if (avx_cpu && osxsave) {
-            #if defined(_MSC_VER)
-                    uint64_t xcr0 = _xgetbv(0);
             #else
-                    uint32_t eax, edx;
-                    asm("xgetbv" : "=a"(eax), "=d"(edx) : "c"(0));
-                    uint64_t xcr0 = (static_cast<uint64_t>(edx) << 32) | eax;
+                __get_cpuid(1, &eax, &ebx, &ecx, &edx);
+                f.sse2  = (edx & (1 << 26)) != 0;
+                avx_cpu = (ecx & (1 << 28)) != 0;
+                bool osxsave = (ecx & (1 << 27)) != 0;
             #endif
+
+                if (avx_cpu && osxsave) {
+                    uint64_t xcr0 = get_xcr0();
                     avx_os = (xcr0 & 0x6) == 0x6;
                 }
             }
 
             if (nIds >= 7 && avx_cpu && avx_os) {
-                cpuid(info, 7);
+            #if defined(_MSC_VER)
+                __cpuidex(info, 7, 0);
                 f.avx2 = (info[1] & (1 << 5)) != 0;
+            #else
+                __get_cpuid_count(7, 0, &eax, &ebx, &ecx, &edx);
+                f.avx2 = (ebx & (1 << 5)) != 0;
+            #endif
             }
         #elif defined(BLAM_PLATFORM_ARM64)
-            // unsigned long hwcaps = getauxval(AT_HWCAP);
-            // unsigned long hwcaps2 = getauxval(AT_HWCAP2);
-            // f.neon = (hwcaps & HWCAP_ASIMD) != 0;
-            // f.sve = (hwcaps2 & HWCAP2_SVE2) != 0;
-            f.neon = false;
-            f.sve = false;
+            #ifdef BLAM_PLATFORM_LINUX
+                auto hwcaps = getauxval(AT_HWCAP);
+                f.neon = (hwcaps & HWCAP_ASIMD) != 0;
+
+                // SVE is not implemented yet, so we'll just set it to false for now
+                f.sve = false;
+
+                // #ifdef HWCAP2_SVE2
+                // auto hwcaps2 = getauxval(AT_HWCAP2);
+                // f.sve = (hwcaps2 & HWCAP2_SVE2) != 0;
+                // #elif defined(HWCAP_SVE)
+                // f.sve = (hwcaps & HWCAP_SVE) != 0;
+                // #else
+                // f.sve = false;
+                // #endif
+            #elif defined(BLAM_PLATFORM_MACOS)
+                // on macs, NEON is always supported
+                f.neon = true;
+                f.sve = false;
+            #else
+                f.neon = false;
+                f.sve = false;
+            #endif
         #endif
             return f;
         }();
--- src/SIMD/Common.hpp
+++ src/SIMD/Common.hpp
@@ -25,10 +25,12 @@
 
     #if defined(__ARM_NEON)
         #define BLAM_HAS_NEON
+        #include <arm_neon.h>
     #endif
 
     #if defined(__ARM_FEATURE_SVE)
         #define BLAM_HAS_SVE
+        #include <arm_sve.h>
     #endif
 #endif
 
